18c63bbefa5c4fda473890e6b1ca85e2a09d3204
Alex Sander <alexsandermanhes@gmail.com>
2024-12-05 12:56:53 -0300

    Array de objetos

---
+++ 02-04-array-de-objetos/script.js
@@ -0,0 +1,18 @@
+var person1 ={
+    name: 'Alex',
+    age: 25
+}
+var person2 ={
+    name: 'Daniel',
+    age: 24
+}
+var person3 ={
+    name: 'Caio',
+    age: 23
+}
+
+var list = [person1, person2, person3]
+
+for (var person of list){
+    console.log(person)
+}
